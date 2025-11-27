5a18255a548e1d951e4961b35a1429ee285c4980
Sepehr <sepehr2018.f@gmail.com>
2025-11-27 22:26:54 +0330

    Added Data Base Connection

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask import g
 
 DB_NAME = "main.db"
 
@@ -28,4 +29,29 @@ def init_tables(conn):
 def init_db():
     conn = sqlite3.connect(DB_NAME)
     init_tables(conn)
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(DB_NAME)
+    return g.db
+
+
+def close_db(error=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+
+def get_all_tags():
+    db = get_db()
+    return db.execute("select * from tags").fetchall()
+
+
+def insert_tags(name):
+    db = get_db()
+    db.execute("insert into tags (name) values (?)", (name,) )
+    db.commit()
+
+
 
--- main.py
+++ main.py
@@ -3,13 +3,28 @@ import database as db
 
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def teardown_db(error):
+    db.close_db(error)
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return db.get_all_tags()
+
+
+@app.route("/new_tag/<tag_name>")
+def new_tag(tag_name):
+    print(tag_name)
+    db.insert_tags(tag_name)
+    return ""
+
+
+
+
+
 
 if __name__ == "__main__":
     with app.app_context():
         db.init_db()
     app.run(debug=True)
-
-
